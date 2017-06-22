89f4eae2441e3581669e667f9633e52accbbc886
Usha <usha.macdonald@gmail.com>
2017-06-22 18:32:44 +1200

    added function three

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
    one()
   two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -27,5 +28,10 @@ function two () {
 }
 
 // CREATE FUNCTION three HERE
+
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('pink')
+}
 
 // CREATE FUNCTION makeVisible HERE
