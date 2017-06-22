3f039447f9f2bb513abcee88d0c37d248b5f59a9
Usha <usha.macdonald@gmail.com>
2017-06-22 18:41:25 +1200

    added invisible function

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
    one()
   two()
   three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -35,3 +36,12 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+
+
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
+
+
+
