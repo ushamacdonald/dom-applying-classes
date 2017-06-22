a68477b25c7711e92d3a2db43ac397b6a9f162f2
Usha <usha.macdonald@gmail.com>
2017-06-22 18:31:01 +1200

    added function two

--- classes.js
+++ classes.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+   one()
+  two()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -19,6 +20,11 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
